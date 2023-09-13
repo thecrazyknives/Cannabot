4b80d6351864ca64e0ef8574b909cc2abab435c5
thecrazyknives <137094386+thecrazyknives@users.noreply.github.com>
2023-09-12 22:15:31 -0600

    Create obtenerGramaje.py

---
+++ obtenerGramaje.py
@@ -0,0 +1,15 @@
+
+
+
+def obtenerGramaje(consumo, dosis, peso):
+    mg = 10 if consumo == "bajo" else (20 if consumo == "medio" else 30)
+    
+    
+
+
+#   baja            1 - 2.5
+#   media baja      2.5 - 15
+#   media           15 - 30
+#   media alta      30 - 50
+#   alta            50 +
+
